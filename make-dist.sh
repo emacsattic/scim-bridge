c239196257d0536d7c9a6677ee42ac865b974a5c
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2010-03-04 22:00:08 +0900

    make-dist.sh: Added many options
    usage: ./make-dist.sh [OPTION]...
      -c  edit debian/changelog
      -b  build deb package
      -S  build source package
      -p  build deb package using pbuilder
      -u  add -us -uc options to debuild
      -h  display this help and exit

--- make-dist.sh
+++ make-dist.sh
@@ -2,18 +2,87 @@
 # Copyright (c) 2010 and onwards, S. Irie
 # This program is distributed under the MIT Licence.
 
+PACKAGE="scim-bridge-el"
+VERSION=$(sed -n 's/^(defconst scim-mode-version "\(.*\)")$/\1/p' scim-bridge-dev.el)
+
 EL_DEV="scim-bridge-dev.el"
 EL_MAIN="scim-bridge.el"
 DBGREGEXP="^;*\t* *(if scim-debug ("
 
-FILES=("README" "COPYING" "ChangeLog" "l10n" "l10n-dev")
+FILES=("README" "COPYING" "ChangeLog" "l10n" "l10n-dev" "debian")
 
-VERSION=$(sed -n 's/^(defconst scim-mode-version "\(.*\)")$/\1/p' scim-bridge-dev.el)
-ARCHDIR="scim-bridge-el-${VERSION}"
+
+# Command line options =================
+
+DCH="no"
+DEBUILD="no"
+BLDTYPE="deb"
+BLDFLAGS=""
+
+while getopts :hcbSpu OPTION
+do
+    case $OPTION in
+	h)
+	    echo "usage: $0 [OPTION]..."
+	    echo "  -c  edit debian/changelog"
+	    echo "  -b  build deb package"
+	    echo "  -S  build source package"
+	    echo "  -p  build deb package using pbuilder"
+	    echo "  -u  add -us -uc options to debuild"
+	    echo "  -h  display this help and exit"
+	    exit
+	    ;;
+	c)
+	    DCH="yes"
+	    ;;
+	b)
+	    DEBUILD="yes"
+	    ;;
+	S)
+	    if [ "$BLDTYPE" == "pbuild" ]; then
+		echo "$0: options -p and -S can't be used together." >&2
+		exit 1
+	    fi
+
+	    DEBUILD="yes"
+	    BLDTYPE="src"
+	    ;;
+	p)
+	    if [ "$BLDTYPE" == "src" ]; then
+		echo "$0: options -S and -p can't be used together." >&2
+		exit 1
+	    fi
+
+	    DEBUILD="yes"
+	    BLDTYPE="pbuild"
+	    ;;
+	u)
+
+	    BLDFLAGS="${BLDFLAGS} -us -uc"
+	    ;;
+	?)
+	    echo "$0: invalid option -- $OPTARG" >&2
+	    echo "Try \`$0 -h' for more information." >&2
+	    exit 1
+	    ;;
+    esac
+done
+
+
+# Paths ================================
+
+WORKDIR="v${VERSION}"
+ARCHDIR="${WORKDIR}/${PACKAGE}-${VERSION}"
 ARCHFILE="${ARCHDIR}.tar.gz"
+ARDEBDIR="${ARCHDIR}/debian"
+ORIGARCH="${WORKDIR}/${PACKAGE}_${VERSION}.orig.tar.gz"
+CURRDIR="$PWD"
 
-rm -rfv $ARCHDIR
-mkdir -v $ARCHDIR
+
+# Locate files =========================
+
+rm -rfv $WORKDIR
+mkdir -pv $ARCHDIR
 
 echo "comment out debug codes in $EL_DEV -> ${ARCHDIR}/${EL_MAIN}"
 sed "s/\(${DBGREGEXP}\)/;#\1/" $EL_DEV > ${ARCHDIR}/${EL_MAIN}
@@ -22,5 +91,50 @@ for i in ${FILES[@]}; do
     cp -rpv $i $ARCHDIR
 done
 
-echo "create archve $ARCHFILE"
-tar cvzf $ARCHFILE $ARCHDIR
+for i in $(find $ARCHDIR -regex '.*\(~\|\.~[0-9]+~\)'); do
+    rm -fv $i      # Delete backup files
+done
+
+
+# Edit changelog =======================
+
+if [ "$DCH" == "yes" ]; then
+    cd $ARDEBDIR
+    dch -v ${VERSION}
+    cd $CURRDIR
+
+    cp -pv --backup=t ${ARDEBDIR}/changelog debian/changelog
+fi
+
+
+# Create archive =======================
+
+echo "create archive $ARCHFILE"
+cd $WORKDIR
+tar cvzf $(basename $ARCHFILE) $(basename $ARCHDIR)
+cd $CURRDIR
+
+
+# Build deb package  ===================
+
+if [ ! "$DEBUILD" == "yes" ]; then
+    exit 0
+fi
+
+cp -pv $ARCHFILE $ORIGARCH
+
+cd $ARCHDIR
+
+case $BLDTYPE in
+    deb)
+	debuild ${BLDFLAGS}
+	;;
+    src)
+	debuild -S ${BLDFLAGS}
+	;;
+    pbuild)
+	debuild -S ${BLDFLAGS} && sudo pbuilder build ../*.dsc
+	;;
+esac
+
+cd $CURRDIR
